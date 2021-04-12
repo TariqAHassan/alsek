6f8d9a22ffbfd5eda5f6221c46fb28a3061f70d1
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-12 16:26:01 -0300

    Implement task and trigger task

---
+++ alsek/core/task.py
@@ -0,0 +1,564 @@
+"""
+
+    Task
+
+"""
+from __future__ import annotations
+
+import logging
+from inspect import signature
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Type, Union
+
+from apscheduler.job import Job
+from apscheduler.jobstores.base import JobLookupError
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.date import DateTrigger
+from apscheduler.triggers.interval import IntervalTrigger
+
+from alsek import (
+    DEFAULT_MAX_RETRIES,
+    DEFAULT_MECHANISM,
+    DEFAULT_QUEUE,
+    DEFAULT_TASK_TIMEOUT,
+)
+from alsek._utils.printing import auto_repr
+from alsek.core.backoff import Backoff, ExponentialBackoff
+from alsek.core.broker import Broker
+from alsek.core.message import Message
+from alsek.exceptions import SchedulingError, ValidationError
+from alsek.storage.result import ResultStore
+
+log = logging.getLogger(__name__)
+
+SUPPORTED_MECHANISMS: Tuple[str, ...] = ("process", "thread")
+
+
+def _expects_message(function: Callable[..., Any]) -> bool:
+    parameters = signature(function).parameters
+    if "message" in parameters:
+        if isinstance(parameters["message"].annotation, str):
+            return parameters["message"].annotation == Message.__name__
+        else:
+            return True
+    else:
+        return False
+
+
+def _collapse_callbacks(callbacks: Tuple[Message, ...]) -> Message:
+    # Note: this function operates 'in place' on the items in
+    # `callbacks`. This is done so that callback information
+    # will become available on the original messages.
+    if any(c.callback_message_data for c in callbacks):
+        raise AttributeError(f"Existing callbacks in {callbacks}")
+
+    for i in range(1, len(callbacks))[::-1]:
+        callbacks[i - 1].update(callback_message_data=callbacks[i].data)
+    return callbacks[0]
+
+
+def _parse_callback(callback: Union[Message, Tuple[Message, ...]]) -> Message:
+    if isinstance(callback, Message):
+        return callback
+    else:
+        return _collapse_callbacks(callback)
+
+
+class _MultiSubmit:
+    def __init__(self, message: Message, broker: Broker, **options: Any) -> None:
+        self._message = message
+        self.broker = broker
+        self.options = options
+
+        self.first: bool = True
+
+    @property
+    def message(self) -> Message:
+        return (
+            self._message
+            if self.first
+            else self._message.duplicate().update(progenitor=self._message.uuid)
+        )
+
+    def __call__(self) -> None:
+        self.broker.submit(self.message, **self.options)
+        self.first = False
+
+
+class Task:
+    """Alsek Task.
+
+    Args:
+        function (callable): function to use for the main operation
+        broker (Broker): an Alsek broker
+        name (str, optional): the name of the task. If ``None``,
+            the class name will be used.
+        queue (str): the name of the queue to generate the task on
+        priority (int): priority of the task. Tasks with lower values
+            will be executed before tasks with higher values.
+        timeout (int): the maximum amount of time (in milliseconds)
+            this task is permitted to run.
+        max_retries (int, optional): maximum number of allowed retries
+        backoff (Backoff): backoff algorithm and parameters to use when computing
+            delay between retries
+        result_store (ResultStore): store for persisting task results
+        mechanism (str): mechanism for executing the task. Must
+            be either "process" or "thread".
+
+    Notes:
+        * ``do_retry()`` can be overridden in cases where ``max_retries``
+           is not sufficiently complex to determine if a retry should occur.
+
+    Warning:
+        * Timeouts are not supported for ``mechanism='thread'`` on Python
+          implementations other than CPython.
+
+    """
+
+    def __init__(
+        self,
+        function: Callable[..., Any],
+        broker: Broker,
+        name: Optional[str] = None,
+        queue: str = DEFAULT_QUEUE,
+        priority: int = 0,
+        timeout: int = DEFAULT_TASK_TIMEOUT,
+        max_retries: Optional[int] = DEFAULT_MAX_RETRIES,
+        backoff: Backoff = ExponentialBackoff(),
+        result_store: Optional[ResultStore] = None,
+        mechanism: str = DEFAULT_MECHANISM,
+    ) -> None:
+        self.function = function
+        self.broker = broker
+        self.queue = queue
+        self.priority = priority
+        self.timeout = timeout
+        self._name = name
+        self.max_retries = max_retries
+        self.backoff = backoff
+        self.result_store = result_store
+        self.mechanism = mechanism
+
+        if priority < 0:
+            raise ValueError("`priority` must be greater than or equal to zero")
+        elif mechanism not in SUPPORTED_MECHANISMS:
+            raise ValueError(f"Unsupported mechanism '{mechanism}'")
+
+        self._deferred: bool = False
+
+    def _get_serializable_task(self) -> Task:
+        return self
+
+    @property
+    def name(self) -> str:
+        """Name of the task."""
+        return self._name if self._name else self.function.__name__
+
+    def __repr__(self) -> str:
+        return auto_repr(
+            self,
+            function=self.function,
+            broker=self.broker,
+            name=self.name,
+            queue=self.queue,
+            priority=self.priority,
+            max_retries=self.max_retries,
+            backoff=self.backoff,
+            mechanism=self.mechanism,
+            deferred_mode="enabled" if self._deferred else "disabled",
+        )
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        return self.function(*args, **kwargs)
+
+    @property
+    def deferred(self) -> bool:
+        """Whether or not deferred mode is currently enabled."""
+        return self._deferred
+
+    def defer(self) -> Task:
+        """Enter "deferred" mode.
+
+        Do not submit the next message created by ``generate()``
+        to the broker.
+
+        Returns:
+            task (Task): the current task
+
+        Warning:
+            * Deferred mode is automatically cancelled by ``generate()``
+              prior to it returning.
+
+        """
+        self._deferred = True
+        return self
+
+    def cancel_defer(self) -> Task:
+        """Cancel "deferred" mode.
+
+        Returns:
+            task (Task): the current task
+
+        """
+        self._deferred = False
+        return self
+
+    def _submit(self, message: Message, **options: Any) -> None:
+        self.broker.submit(message, **options)
+
+    def _validate(self, store_result: bool) -> None:
+        if store_result and not self.result_store:
+            raise ValidationError(f"Cannot store result, `result_store` not set")
+
+    def generate(
+        self,
+        args: Optional[Union[List[Any], Tuple[Any, ...]]] = None,
+        kwargs: Optional[Dict[Any, Any]] = None,
+        metadata: Optional[Dict[Any, Any]] = None,
+        store_result: bool = False,
+        result_ttl: Optional[int] = None,
+        uuid: Optional[str] = None,
+        timeout_override: Optional[int] = None,
+        delay: Optional[int] = None,
+        previous_result: Any = None,
+        callback: Optional[Union[Message, Tuple[Message, ...]]] = None,
+        submit: bool = True,
+        **options: Any,
+    ) -> Message:
+        """Generate an instance of the task for processing.
+
+        This method generates a new message for the task and
+        submit it to the broker.
+
+        Args:
+            args (list, tuple, optional): positional arguments to pass to ``function``
+            kwargs (dict, optional): keyword arguments to pass to ``function``
+            metadata (dict, optional): a dictionary of user-defined message metadata.
+                This can store any data types supported by the backend's seralizer.
+            store_result (bool): if ``True`` persist the result to a result store.
+            result_ttl (int, optional): time to live (in milliseconds) for the
+                result in the result store. If ``None``, the result will be
+                persisted indefinitely. Note that if ``store_result`` is ``False``
+                this parameter will be ignored.
+            uuid (str, optional): universal unique identifier for the message.
+                If ``None``, one will be generated automatically.
+            timeout_override (int, optional): override the default maximum runtime
+                (in milliseconds) for instances of this task.
+            set maximum amount of time (in milliseconds)
+                this message is permitted to run. This will override the default
+                for the task.
+            delay (int, optional): delay before message is ready
+            previous_result (Any): result of a previous task.
+            callback (Message, Tuple[Message, ...], optional): one ore more messages
+                to be submitted to the broker after the prceeding messeage has been
+                successfully processed by a worker.
+            submit (bool): if ``True`` submit the task to the broker
+            options (Keyword Args): options to use when submitting
+                the message via the broker. See ``Broker.submit()``.
+
+        Returns:
+            message (Message): message generated for the task
+
+        Warning:
+            * ``submit`` is overridden to ``False`` if deferred mode is active
+            * ``uuid`` is refreshed after the first event when using a trigger.
+
+        """
+        self._validate(store_result)
+
+        message = Message(
+            task_name=self.name,
+            queue=self.queue,
+            args=args,
+            kwargs=kwargs,
+            metadata=metadata,
+            store_result=store_result,
+            result_ttl=result_ttl,
+            uuid=uuid,
+            timeout=timeout_override or self.timeout,
+            delay=delay,
+            previous_result=previous_result,
+            callback_message_data=_parse_callback(callback).data if callback else None,
+            backoff_settings=self.backoff.settings,
+            mechanism=self.mechanism,
+        )
+        if self._deferred:
+            self.cancel_defer()
+        elif submit:
+            self._submit(message, **options)
+        return message
+
+    def pre_op(self, message: Message) -> None:
+        """Operation to perform before running ``op``.
+
+        Args:
+            message (Message): message ``op`` will be run against.
+
+        Returns:
+            None
+
+        """
+
+    def op(self, message: Message) -> Any:
+        """Pass ``message`` data to ``function`` for processing.
+
+        Args:
+            message (Message): message to perform the operation against
+
+        Returns:
+            result (Any): output of the function
+
+        Notes:
+            * If, and only if, the signature of ``function`` contains
+              a ``message`` parameter, ``message`` itself will be passed
+              along with any ``args`` and ``kwargs`` contained in the message.
+
+        Warning:
+            * ``message`` will not be passed in cases where a "message"
+              exists in ``message.kwargs``.
+
+        """
+        if "message" not in message.kwargs and _expects_message(self.function):
+            return self.function(*message.args, **message.kwargs, message=message)
+        else:
+            return self.function(*message.args, **message.kwargs)
+
+    def post_op(self, message: Message, result: Any) -> None:
+        """Operation to perform after running ``op``.
+
+        Args:
+            message (Message): message ``op()`` was run against
+            result (Any): output of ``op()``
+
+        Returns:
+            None
+
+        """
+
+    def execute(self, message: Message) -> Any:
+        """Execute the task against a message.
+
+        Args:
+            message (Message): message to process
+
+        Returns:
+            result (Any): output of ``op()``
+
+        """
+        self.pre_op(message)
+        result = self.op(message)
+        self.post_op(message, result=result)
+        return result
+
+    def do_retry(self, message: Message, exception: BaseException) -> bool:  # noqa
+        """Whether or not a failed task should be retried.
+
+        Args:
+            message (Message): message which failed
+            exception (BaseException): the exception which was raised
+
+        Returns:
+            bool
+
+        """
+        return self.max_retries is None or message.retries < self.max_retries
+
+
+class TriggerTask(Task):
+    """Triggered Task.
+
+    Args:
+        function (callable): function to use for the main operation
+        trigger (CronTrigger, DateTrigger, IntervalTrigger, optional): trigger
+            for task execution.
+        **kwargs (Keyword Args): Keyword arguments to pass ``Task()``.
+
+    Warnings:
+        * The signature of ``function`` cannot contain parameters
+
+    """
+
+    def __init__(
+        self,
+        function: Callable[..., Any],
+        trigger: Optional[Union[CronTrigger, DateTrigger, IntervalTrigger]] = None,
+        **kwargs: Any,
+    ) -> None:
+        if signature(function).parameters:
+            raise SchedulingError(
+                "Unable to schedule trigger tasks which accept parameters"
+            )
+        super().__init__(function, **kwargs)
+        self.trigger = trigger
+
+        self.scheduler = BackgroundScheduler()
+
+    def _get_serializable_task(self) -> TriggerTask:
+        new = TriggerTask(
+            function=self.function,
+            trigger=self.trigger,
+            broker=self.broker,
+            name=self.name,
+            queue=self.queue,
+            priority=self.priority,
+            max_retries=self.max_retries,
+            backoff=self.backoff,
+            result_store=self.result_store,
+            mechanism=self.mechanism,
+        )
+        new.scheduler = None  # schedulers cannot be serialized
+        return new
+
+    @property
+    def _job(self) -> Optional[Job]:
+        return self.scheduler.get_job(self.name)
+
+    def _submit(self, message: Message, **options: Any) -> None:
+        if self._job:
+            raise SchedulingError("Task already scheduled")
+
+        self.scheduler.add_job(
+            _MultiSubmit(message, broker=self.broker, options=options),
+            trigger=self.trigger,
+            id=self.name,
+        )
+
+    @property
+    def generated(self) -> bool:
+        """If the task has been generated."""
+        return bool(self._job)
+
+    def clear(self) -> None:
+        """Clear the currently scheduled task.
+
+        Returns:
+            None
+
+        Raises
+            * AttributeError: if a task has not yet
+                been generated
+
+        """
+        try:
+            self.scheduler.remove_job(self.name)
+        except JobLookupError:
+            raise AttributeError("Task not generated")
+
+    def pause(self) -> None:
+        """Pause the underlying scheduler.
+
+        Returns:
+            None
+
+        """
+        self.scheduler.pause()
+
+    def resume(self) -> None:
+        """Resume the underlying scheduler.
+
+        Returns:
+            None
+
+        """
+        self.scheduler.resume()
+
+    def shutdown(self) -> None:
+        """Shutdown the underlying scheduler.
+
+        Returns:
+            None
+
+        """
+        self.scheduler.shutdown()
+
+
+def _parse_base_task(
+    base_task: Optional[Type[Task]],
+    trigger: Optional[Union[CronTrigger, DateTrigger, IntervalTrigger]],
+) -> Type[Task]:
+    if base_task:
+        return base_task
+    elif trigger:
+        return TriggerTask
+    else:
+        return Task
+
+
+def task(
+    broker: Broker,
+    name: Optional[str] = None,
+    queue: str = DEFAULT_QUEUE,
+    priority: int = 0,
+    timeout: int = DEFAULT_TASK_TIMEOUT,
+    max_retries: int = DEFAULT_MAX_RETRIES,
+    backoff: Backoff = ExponentialBackoff(),
+    trigger: Optional[Union[CronTrigger, DateTrigger, IntervalTrigger]] = None,
+    result_store: Optional[ResultStore] = None,
+    mechanism: str = DEFAULT_MECHANISM,
+    base_task: Optional[Type[Task]] = None,
+) -> Callable[..., Task]:
+    """Wrapper for task construction.
+
+    Args:
+        broker (Broker): an Alsek broker
+        name (str, optional): the name of the task. If ``None``,
+            the class name will be used.
+        queue (str): the name of the queue to generate the task on
+        priority (int): priority of the task. Tasks with lower values
+            will be executed before tasks with higher values.
+        timeout (int): the maximum amount of time (in milliseconds)
+            this task is permitted to run.
+        max_retries (int, optional): maximum number of allowed retries
+        backoff (Backoff): backoff algorithm and parameters to use when computing
+            delay between retries
+        trigger (CronTrigger, DateTrigger, IntervalTrigger, optional): trigger
+            for task execution.
+        result_store (ResultStore): store for persisting task results
+        mechanism (str): mechanism for executing the task. Must
+            be either "process" or "thread".
+        base_task (Type[Task]): base to use for task constuction.
+            If ``None``, a base task will be selected automatically.
+
+    Returns:
+        wrapper (callable): task-wrapped function
+
+    Raises:
+        * ValueError: if a ``trigger`` and not supported by ``base_task``
+
+    Examples:
+        >>> from alsek.core.broker import Broker
+        >>> from alsek.core.task import task
+        >>> from alsek.storage.backends.disk import DiskCacheBackend
+
+        >>> backend = DiskCacheBackend()
+        >>> broker = Broker(backend)
+
+        >>> @task(broker)
+        ... def add(a: int, b: int) -> int:
+        ...     return a + b
+
+    """
+    parsed_base_task = _parse_base_task(base_task, trigger=trigger)
+    if trigger and "trigger" not in signature(parsed_base_task.__init__).parameters:
+        raise ValueError(f"Trigger not supported by {parsed_base_task}")
+
+    def wrapper(function: Callable[..., Any]) -> Task:
+        return parsed_base_task(  # type: ignore
+            function=function,
+            name=name,
+            broker=broker,
+            queue=queue,
+            priority=priority,
+            timeout=timeout,
+            max_retries=max_retries,
+            backoff=backoff,
+            mechanism=mechanism,
+            result_store=result_store,
+            **(  # noqa (handled above)
+                dict(trigger=trigger)
+                if trigger in signature(parsed_base_task.__init__).parameters
+                else dict()
+            ),
+        )
+
+    return wrapper
