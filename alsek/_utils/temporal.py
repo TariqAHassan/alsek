fbd508f1e30a54988745d26586ad3f0fb7b9576d
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-12 16:22:50 -0300

    Add temporal utils

---
+++ alsek/_utils/temporal.py
@@ -0,0 +1,40 @@
+"""
+
+    Temporal Utils
+
+"""
+import time
+from datetime import datetime
+
+
+def utcnow_timestamp_ms() -> int:
+    """UTC timestamp in milliseconds.
+
+    Returns:
+        timestamp (int): UTC time in milliseconds
+
+    """
+    return int(datetime.utcnow().timestamp() * 1000)
+
+
+def utcfromtimestamp_ms(timestamp: int) -> datetime:
+    """Construct datetime object from UTC timestamp in milliseconds.
+
+    Args:
+        timestamp (int): UTC time in milliseconds
+
+    Returns:
+        datetime
+
+    """
+    return datetime.utcfromtimestamp(timestamp / 1000)
+
+
+def time_ms() -> int:
+    """Get the current time since the Epoch in milliseconds.
+
+    Returns:
+        time (int): current time in milliseconds
+
+    """
+    return int(time.time() * 1000)
