0d218469d02f405d5a8256e123abb0b106ffe958
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-15 17:32:03 -0300

    Add sorting tests

---
+++ tests/_utils/test_sorting.py
@@ -0,0 +1,26 @@
+"""
+
+    Test Sorting
+
+"""
+from typing import Any, Callable, Dict
+
+import pytest
+
+from alsek._utils.sorting import dict_sort
+
+
+@pytest.mark.parametrize(
+    "input_,key,expected",
+    [
+        ({"a": 1, "b": 2}, lambda k: k, {"a": 1, "b": 2}),
+        ({2: "a", 1: "b"}, lambda k: k, {1: "b", 2: "a"}),
+        ({2: "a", 1: "b"}, lambda k: -k, {2: "a", 1: "b"}),
+    ],
+)
+def test_dict_sort(
+    input_: Dict[Any, Any],
+    key: Callable[[Any], Any],
+    expected: Dict[Any, Any],
+) -> None:
+    assert dict_sort(input_, key) == expected
