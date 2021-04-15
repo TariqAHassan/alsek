6a04bbceb35d2ae0bd828c340d7df72afdd130ba
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-15 20:56:11 -0300

    Test exceptions

---
+++ tests/test_exceptions.py
@@ -0,0 +1,16 @@
+"""
+
+    Test Exceptions
+
+"""
+import pytest
+
+from alsek.exceptions import AlsekError
+
+
+@pytest.mark.parametrize("exception", AlsekError.__subclasses__())
+def test_exceptions(exception: BaseException) -> None:
+    try:
+        raise exception()
+    except BaseException as error:
+        assert isinstance(error, AlsekError)
