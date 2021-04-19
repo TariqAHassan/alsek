1657f40e60072476ff3aa96cc71c76ab6b17c04c
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-18 21:01:21 -0300

    Add string utils

--- alsek/storage/backends/disk.py
+++ alsek/storage/backends/disk.py
@@ -9,6 +9,7 @@ from typing import Any, Callable, Iterable, Optional, Union, cast
 
 from alsek import DEFAULT_NAMESPACE
 from alsek._utils.printing import auto_repr
+from alsek._utils.string import name_matcher
 from alsek.storage.backends import Backend, LazyClient
 from alsek.storage.serialization import JsonSerializer, Serializer
 
@@ -18,47 +19,6 @@ except ImportError:
     raise ImportError(
         "diskcache is not installed. Please install in order to use DiskCacheBackend."
     )
-
-
-def name_matcher(pattern: Optional[str], name: str) -> bool:
-    """Determine if ``pattern`` matches ``name``.
-
-    This function supports the following forms
-    of matching:
-
-        * none (all names considered to be matches)
-        * direct (pattern == name)
-        * leading astrix (e.g., "*pattern") for tail matching
-        * trailing astrix (e.g., "pattern*") for head matching
-        * leading and trailing astrix (e.g., "*pattern*") for substring matching
-
-    Args:
-        pattern (str, optional): the pattern to match against
-        name (str): the target name.
-
-    Returns:
-        bool
-
-    Examples:
-        >>> assert name_matcher(None, name="cats")
-        >>> assert name_matcher("cats", name="cats")
-        >>> assert name_matcher("*cats", name="happy_cats")
-        >>> assert name_matcher("cats*", name="cats_happy")
-        >>> assert name_matcher("*cats*", name="many cats happy")
-
-    """
-    if pattern is None:
-        return True
-    elif pattern == name:
-        return True
-    elif pattern.startswith("*") and pattern.endswith("*"):
-        return pattern.strip("*") in name
-    elif pattern.startswith("*"):
-        return name.endswith(pattern.lstrip("*"))
-    elif pattern.endswith("*"):
-        return name.startswith(pattern.rstrip("*"))
-    else:
-        return False
 
 
 class DiskCacheBackend(Backend):
