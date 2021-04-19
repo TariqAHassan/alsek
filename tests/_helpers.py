638dffd86dd5c65d6ab82b8333b03670a61884db
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-18 23:14:54 -0300

    Add test helpers

---
+++ tests/_helpers.py
@@ -0,0 +1,33 @@
+"""
+
+    Test Helpers
+    ~~~~~~~~~~~~
+
+"""
+from typing import Iterable, Any, Callable, Tuple, List
+
+
+def expand_params_factory(
+    expander: Tuple[Any, ...],
+) -> Callable[..., List[Tuple[Any, ...]]]:
+    """Factory to generate a function which can expand testing parameters.
+
+    Args:
+        expander (Tuple[Any]): a tuple of values to expand ``params`` by
+
+    Returns:
+        engine (callable): engine to add all items in ``expander`` to ``params``.
+
+    """
+
+    def engine(*params: Any) -> List[Tuple[Any, ...]]:
+        expanded = list()
+        for s in expander:
+            for p in params:
+                if isinstance(p, (list, tuple)):
+                    expanded.append((*p, s))
+                else:
+                    expanded.append((p, s))
+        return expanded
+
+    return engine
