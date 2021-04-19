974dc13a6bbdb78a5595407d09dc0b1243a28b26
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-18 21:56:52 -0300

    Add test_serialization.py

---
+++ tests/storage/test_serialization.py
@@ -0,0 +1,27 @@
+"""
+
+    Test Serialization
+
+"""
+import pytest
+from alsek.storage.serialization import JsonSerializer, Serializer
+from tests._helpers import expand_params_factory
+from typing import Any, List, Tuple
+
+_ALL_SERIALIZERS = (JsonSerializer(),)
+_expand_to_all_serializers = expand_params_factory(_ALL_SERIALIZERS)
+
+
+@pytest.mark.parametrize(
+    "value,seralizer",
+    _expand_to_all_serializers(
+        1,
+        1.0,
+        "string",
+        [["list", "of", "strings"]],
+        {"a": 1},
+        {"a": [1, 2, 3]},
+    ),
+)
+def test_seralizer(value: Any, seralizer: Serializer) -> None:
+    assert seralizer.reverse(seralizer.forward(value)) == value
