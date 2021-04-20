139e1ad89d079f8da183a377b52426100036c4d8
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-19 21:25:19 -0300

    Add concurrency tests

---
+++ tests/core/test_concurrency.py
@@ -0,0 +1,67 @@
+"""
+
+    Test Concurrency
+
+"""
+import pytest
+from socket import gethostname
+from alsek.core.concurrency import Lock
+from alsek.storage.backends import Backend
+
+
+def test_repr(rolling_backend: Backend) -> None:
+    lock = Lock("lock", backend=rolling_backend)
+    assert isinstance(repr(lock), str)
+
+
+def test_long_name(rolling_backend: Backend) -> None:
+    lock = Lock("lock", backend=rolling_backend)
+    assert lock.long_name == "locks:lock"
+
+
+@pytest.mark.parametrize("do_acquire", [True, False])
+def test_holder(do_acquire: bool, rolling_backend: Backend) -> None:
+    lock = Lock("lock", backend=rolling_backend)
+
+    if do_acquire:
+        lock.acquire()
+        assert lock.holder == gethostname()
+    else:
+        assert lock.holder is None
+
+
+@pytest.mark.parametrize("do_acquire", [True, False])
+def test_held(do_acquire: bool, rolling_backend: Backend) -> None:
+    lock = Lock("lock", backend=rolling_backend)
+
+    if do_acquire:
+        lock.acquire()
+        assert lock.held
+    else:
+        assert not lock.held
+
+
+def test_acquire(rolling_backend: Backend) -> None:
+    lock = Lock("lock", backend=rolling_backend)
+    lock.acquire()
+    assert lock.held
+
+
+def test_release(rolling_backend: Backend) -> None:
+    lock = Lock("lock", backend=rolling_backend)
+    lock.acquire()
+    assert lock.held
+    lock.release()
+    assert not lock.held
+
+
+@pytest.mark.parametrize("auto_release", [True, False])
+def test_context(auto_release: bool, rolling_backend: Backend) -> None:
+    with Lock("lock", backend=rolling_backend, auto_release=auto_release) as lock:
+        lock.acquire()
+        assert lock.held
+
+    if auto_release:
+        assert not lock.held
+    else:
+        assert lock.held
