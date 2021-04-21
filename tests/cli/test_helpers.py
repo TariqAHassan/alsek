0146c5aab089643732333592fc6dc470e6de6523
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-21 02:30:10 -0300

    Add test for `parse_logging_level()`

--- tests/cli/test_helpers.py
+++ tests/cli/test_helpers.py
@@ -3,11 +3,32 @@
     Test CLI Helpers
 
 """
+import logging
 from typing import Set
 
 import pytest
 
-from alsek.cli._helpers import collect_tasks
+from alsek.cli._helpers import collect_tasks, parse_logging_level
+
+
+@pytest.mark.parametrize(
+    "debug,verbose,expected",
+    [
+        # Debug
+        (True, False, logging.DEBUG),
+        (True, True, logging.DEBUG),
+        # Info (verbose)
+        (False, True, logging.INFO),
+        # All other cases
+        (False, False, logging.ERROR),
+    ],
+)
+def test_parse_logging_level(
+    debug: bool,
+    verbose: bool,
+    expected: int,
+) -> None:
+    assert parse_logging_level(debug, verbose=verbose) is expected
 
 
 @pytest.mark.parametrize(
