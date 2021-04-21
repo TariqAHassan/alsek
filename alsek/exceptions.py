71296721a920917011b5cf0fc77207d56ca2f493
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-04-21 18:18:25 -0300

    Fix order

--- alsek/exceptions.py
+++ alsek/exceptions.py
@@ -9,32 +9,32 @@ class AlsekError(Exception):
     """Base Alsek error."""
 
 
-class MessageDoesNotExistsError(AlsekError):
-    """Message does not exists in backend."""
-
-
 class MessageAlreadyExistsError(AlsekError):
     """Message already exists in backend."""
 
 
-class TaskNameCollisionError(AlsekError):
-    """Duplicate task detected."""
-
-
-class NoTasksFoundError(AlsekError):
-    """No tasks found."""
+class MessageDoesNotExistsError(AlsekError):
+    """Message does not exists in backend."""
 
 
 class MultipleBrokersError(AlsekError):
     """Multiple brokers in use."""
 
 
-class SchedulingError(AlsekError):
-    """Error scheduling work."""
+class NoTasksFoundError(AlsekError):
+    """No tasks found."""
 
 
 class ValidationError(AlsekError):
     """Data validation failed."""
+
+
+class SchedulingError(AlsekError):
+    """Error scheduling work."""
+
+
+class TaskNameCollisionError(AlsekError):
+    """Duplicate task detected."""
 
 
 class TerminationError(AlsekError):
