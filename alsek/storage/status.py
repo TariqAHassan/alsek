cab779707f8b195243916afaacdd39daf0e0cc69
TariqAHassan <TariqAHassan@users.noreply.github.com>
2021-05-28 03:03:39 -0300

    Add docstrings and check for `.delete()`

--- alsek/storage/status.py
+++ alsek/storage/status.py
@@ -7,6 +7,7 @@ from enum import Enum
 from typing import Optional
 
 from alsek.core.message import Message
+from alsek.exceptions import ValidationError
 from alsek.storage.backends import Backend
 
 
@@ -18,6 +19,9 @@ class TaskStatus(Enum):
     RETRYING = 3
     FAILED = 4
     SUCCEEDED = 5
+
+
+TERMINAL_TASK_STAUSES = (TaskStatus.FAILED, TaskStatus.SUCCEEDED)
 
 
 class StatusStore:
@@ -42,9 +46,28 @@ class StatusStore:
         return f"status:{message.queue}:{message.task_name}:{message.uuid}"
 
     def exists(self, message: Message) -> bool:
+        """Check if a status for ``message`` exists in the backend.
+
+        Args:
+            message (Message): an Alsek message
+
+        Returns:
+            bool
+
+        """
         return self.backend.exists(self.get_storage_name(message))
 
     def set(self, message: Message, status: TaskStatus) -> None:
+        """Set a ``status`` for ``message``.
+
+        Args:
+            message (Message): an Alsek message
+            status (TaskStatus): a status to set
+
+        Returns:
+            None
+
+        """
         self.backend.set(
             self.get_storage_name(message),
             value=status.name,
@@ -52,8 +75,32 @@ class StatusStore:
         )
 
     def get(self, message: Message) -> TaskStatus:
+        """Get the status of ``message``.
+
+        Args:
+            message (Message): an Alsek message
+
+        Returns:
+            status (TaskStatus): the status of ``message``
+
+        """
         status_name = self.backend.get(self.get_storage_name(message))
         return TaskStatus[status_name]
 
-    def delete(self, message: Message) -> None:
+    def delete(self, message: Message, check: bool = True) -> None:
+        """Delete the status of ``message``.
+
+        Args:
+            message (Message): an Alsek message
+            check (bool): check that it is safe to delete the status.
+                This is done by ensuring that the current status of ``message``
+                is a terminal state (i.e., ``TaskStatus.FAILED`` or
+                ``TaskStatus.SUCCEEDED``).
+
+        Returns:
+            None
+
+        """
+        if check and self.get(message) not in TERMINAL_TASK_STAUSES:
+            raise ValidationError(f"Message '{message.uuid}' in a non-terminal state.")
         self.backend.delete(self.get_storage_name(message), missing_ok=False)
