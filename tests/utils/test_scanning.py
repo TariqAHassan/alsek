709e5ca3bbf9b1a38abef6c7c6a3a7fb29413a63
Tariq Hassan <tariqahassan@users.noreply.github.com>
2024-09-27 22:37:37 -0400

    Support string or ModuleType in `collect_tasks()`

--- tests/utils/test_scanning.py
+++ tests/utils/test_scanning.py
@@ -32,12 +32,12 @@ def test_parse_logging_level(
 
 
 @pytest.mark.parametrize(
-    "name,expected",
+    "module,expected",
     [
         ("examples.simple", {"add"}),
         ("examples.ml", {"predict"}),
     ],
 )
-def test_collect_tasks(name: str, expected: Set[str]) -> None:
-    actual = collect_tasks(name)
+def test_collect_tasks(module: str, expected: Set[str]) -> None:
+    actual = collect_tasks(module)
     assert {i.name for i in actual} == expected
