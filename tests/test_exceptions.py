8d9c6b9fd7cb8386ce4b3605be96ae046400c404
Tariq Hassan <tariqahassan@users.noreply.github.com>
2024-10-26 12:46:18 -0400

    Fix

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -6,11 +6,12 @@
 
 import pytest
 
+from typing import Type
 from alsek.exceptions import AlsekError
 
 
 @pytest.mark.parametrize("exception", AlsekError.__subclasses__())
-def test_exceptions(exception: BaseException) -> None:
+def test_exceptions(exception: Type[BaseException]) -> None:
     try:
         raise exception()
     except BaseException as error:
