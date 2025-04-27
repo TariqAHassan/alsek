b5a68bb08c97b2feb4847d71358908ad45fc60cf
TariqAHassan <tariqahassan@users.noreply.github.com>
2025-04-27 03:03:15 -0400

    Fix

--- tests/run.sh
+++ tests/run.sh
@@ -1,18 +1,8 @@
-timeout 45 pytest \
-      --strict-markers \
-      --cov=alsek \
-      --cov-report term-missing \
-      --cov-fail-under 80 \
-      --no-flaky-report \
-      --showlocals \
-      --timeout=250 \
-      -vv | tee pytest_output.txt
-
-# Check the last line for errors or failures
-if tail -n 1 pytest_output.txt | grep -q "errors\|failed=[1-9]"; then
-   echo "Tests failed"
-   exit 1
-else
-   echo "Tests passed (or timed out after completing successfully)"
-   exit 0
-fi
+pytest --strict-markers \
+       --cov=alsek \
+       --cov-report term-missing \
+       --cov-fail-under 80 \
+       --no-flaky-report \
+       --showlocals \
+       --timeout=250 \
+       -vv
