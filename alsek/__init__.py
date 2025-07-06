93d5487c99919a4b384b863ee25f2accac1be04f
TariqAHassan <tariqahassan@users.noreply.github.com>
2025-07-06 13:49:32 -0400

    Bump

--- alsek/__init__.py
+++ alsek/__init__.py
@@ -10,4 +10,4 @@ from alsek.core.message import Message
 from alsek.core.status.standard import StatusTracker
 from alsek.core.task import task
 
-__version__: str = "1.1.0"
+__version__: str = "1.2.0"
