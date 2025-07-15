1e9675f089d2e981ef2a8a28b3898456080db697
TariqAHassan <tariqahassan@users.noreply.github.com>
2025-07-15 01:09:49 -0400

    Conserve any custom `Broker()`, `StatusTracker()` or `ResultStore()` through `serialize()` and `deserialize()`

--- alsek/core/task.py
+++ alsek/core/task.py
@@ -180,15 +180,18 @@ class Task:
         settings = gather_init_params(self, ignore=("broker",))
 
         # Broker
+        settings["broker_class"] = self.broker.__class__
         settings["broker"] = gather_init_params(self.broker, ignore=("backend",))
         settings["broker"]["backend"] = self.broker.backend.encode()
 
         # Status Tracker
         if self.status_tracker:
+            settings["status_tracker_class"] = self.status_tracker.__class__
             settings["status_tracker"] = self.status_tracker.serialize()
 
         # Result Store
         if self.result_store:
+            settings["result_store_class"] = self.result_store.__class__
             settings["result_store"] = self.result_store.serialize()
 
         return dict(task=self.__class__, settings=settings)
@@ -196,23 +199,21 @@ class Task:
     @staticmethod
     def deserialize(data: dict[str, Any]) -> Task:
         def unwind_settings(settings: dict[str, Any]) -> dict[str, Any]:
-            backend_data = dill.loads(settings["broker"]["backend"])
-
             # Broker
-            settings["broker"]["backend"] = backend_data["backend"]._from_settings(
-                backend_data["settings"]
-            )
-            settings["broker"] = Broker(**settings["broker"])
+            broker_class = settings.pop("broker_class")
+            backend_data = dill.loads(settings["broker"]["backend"])
+            settings["broker"]["backend"] = backend_data["backend"].from_settings(backend_data["settings"])  # fmt: skip
+            settings["broker"] = broker_class(**settings["broker"])
 
             # Status Tracker
             if status_tracker_settings := settings.get("status_tracker"):
-                settings["status_tracker"] = StatusTracker.deserialize(status_tracker_settings)  # fmt: skip
+                status_tracker_class = settings.pop("status_tracker_class")
+                settings["status_tracker"] = status_tracker_class.deserialize(status_tracker_settings)  # fmt: skip
 
             # Result Store
             if result_store_settings := settings.get("result_store"):
-                settings["result_store"] = ResultStore.deserialize(
-                    result_store_settings
-                )
+                result_store_class = settings.pop("result_store_class")
+                settings["result_store"] = result_store_class.deserialize(result_store_settings)  # fmt: skip
 
             return settings
 
