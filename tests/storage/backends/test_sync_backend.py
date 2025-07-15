cee2fee93cdb4a3c51f7d29015b84b2efc633c7d
TariqAHassan <tariqahassan@users.noreply.github.com>
2025-07-15 01:05:51 -0400

    Fix name

--- tests/storage/backends/test_sync_backend.py
+++ tests/storage/backends/test_sync_backend.py
@@ -70,7 +70,7 @@ def test_exists(name: str, do_set: bool, rolling_backend: Backend) -> None:
 
 def test_encoding(rolling_backend: Backend) -> None:
     encoded_backend = rolling_backend.encode()
-    decoded_backend = rolling_backend._from_settings(
+    decoded_backend = rolling_backend.from_settings(
         dill.loads(encoded_backend)["settings"]
     )
     # Verify that the reconstruction yielded the same class type
